70ad6f869750009fddc7c000a585bc9144ada34f
sheldonmike <sheldonmike@gmail.com>
2014-12-09 21:06:59 -0500

    Initial commit

---
+++ app/src/main/java/com/rockettrek/interactivestory/model/Choice.java
@@ -0,0 +1,30 @@
+package com.rockettrek.interactivestory.model;
+
+/**
+ * Created by msheldon on 12/2/2014.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
